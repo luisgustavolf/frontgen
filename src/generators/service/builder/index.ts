86c33d8a871e50348d216f3125ecc9efcb908861
Luis Gustavo <luisgustavolf@gmail.com>
2023-06-27 17:01:24 -0300

    geracao de interfaces

--- src/generators/service/builder/index.ts
+++ src/generators/service/builder/index.ts
@@ -11,21 +11,19 @@ export async function generateService(props: IBuilderProps) {
   const baseDir = getBaseDir(props.vendor, props.restResource)
   const rootDir = props.rootDir || process.cwd()
   const destinationFile = `${rootDir}${baseDir}/index.ts`
-
-  if (existsSync(destinationFile)) {
-    await injectIntoExistentServiceFile(props)
-  } else {
-    await createNewServiceFile(props)
-  }
-}
-
-async function createNewServiceFile(props: IBuilderProps) {
-  const baseDir = getBaseDir(props.vendor, props.restResource)
   const context = new RenderContext({ 
     restResource: props.restResource,
     restResourceAction: props.restResourceAction
   })
 
+  if (existsSync(destinationFile)) {
+    await injectIntoExistentServiceFile(baseDir, context)
+  } else {
+    await createNewServiceFile(baseDir, context)
+  }
+}
+
+async function createNewServiceFile(baseDir: string, context: RenderContext) {
   await templateWriter({
     template: {
       file: `${__dirname}/templates/index.ejs`,
@@ -37,15 +35,11 @@ async function createNewServiceFile(props: IBuilderProps) {
     },
     injector: appendAtEndOfFile
   })
+
+  await generateInterfaces(baseDir, context)
 }
 
-async function injectIntoExistentServiceFile(props: IBuilderProps) {
-  const baseDir = getBaseDir(props.vendor, props.restResource)
-  const context = new RenderContext({ 
-    restResource: props.restResource,
-    restResourceAction: props.restResourceAction
-  })
-  
+async function injectIntoExistentServiceFile(baseDir: string, context: RenderContext) {
   await templateWriter({
     template: {
       file: `${__dirname}/templates/imports.ejs`,
@@ -68,5 +62,33 @@ async function injectIntoExistentServiceFile(props: IBuilderProps) {
       fileName: 'index.ts'
     },
     injector: appendAtEndOfServiceClass
+  })
+
+  await generateInterfaces(baseDir, context)
+}
+
+async function generateInterfaces(baseDir: string, context: RenderContext) {
+  await templateWriter({
+    template: {
+      file: `${__dirname}/templates/iData.ejs`,
+      context
+    },
+    destination: {
+      dir: baseDir,
+      fileName: `i${context.getParamName()}.ts`
+    },
+    injector: appendAtEndOfFile
+  })
+
+  await templateWriter({
+    template: {
+      file: `${__dirname}/templates/iResponse.ejs`,
+      context
+    },
+    destination: {
+      dir: baseDir,
+      fileName: `i${context.getResponseName()}.ts`
+    },
+    injector: appendAtEndOfFile
   })
 }
